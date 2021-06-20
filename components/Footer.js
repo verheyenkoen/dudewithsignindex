0603a5f7029f337b042517bd6e4367fb910b8fa6
Koen Verheyen <verheyen.koen@gmail.com>
2021-06-20 09:34:43 +0200

    Smaller footer text size

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.scss'
 export default function Footer() {
   return (
     <footer className={`footer mt-6 has-border-top-width-3`}>
-      <div className={`${styles.content} content pt-2 has-text-centered`}>
+      <div className={`${styles.content} content pt-2 is-size-7 has-text-centered`}>
         <p>WTF needs a copyright?</p>
       </div>
     </footer>
